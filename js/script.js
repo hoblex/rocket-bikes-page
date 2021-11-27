2321e5349b0d21687e834198fce0f91dc172e225
Aleksandr Alekseev <hoblex@gmail.com>
2021-11-27 20:21:17 +0300

    pixel perfect

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
 var pageBody = document.querySelector('.page-body');
-var overlay = document.querySelector('.page-body');
+var headerLinks = pageHeader.querySelectorAll('.header-nav__item-link');
 
 pageHeader.classList.remove('page-header--nojs');
 pageHeader.classList.add('page-header--closed');
@@ -19,4 +19,13 @@ headerToggle.addEventListener('click', function () {
     pageBody.style.height = '';
     pageBody.style.overflowY = '';
   }
+});
+
+headerLinks.forEach((item) => {
+  item.addEventListener('click', function () {
+    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.remove('page-header--opened');
+    pageBody.style.height = '';
+    pageBody.style.overflowY = '';
+  });
 });
