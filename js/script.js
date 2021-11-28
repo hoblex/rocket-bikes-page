7b45460de2709c4966e7392ef27125b087dcb0b8
Aleksandr Alekseev <hoblex@gmail.com>
2021-11-28 21:18:00 +0300

    fix placeholer, add localStorage

--- js/script.js
+++ js/script.js
@@ -4,28 +4,50 @@ var headerToggle = document.querySelector('.page-header__toggle');
 var pageBody = document.querySelector('.page-body');
 var headerLinks = pageHeader.querySelectorAll('.header-nav__item-link');
 
+var headerForm = document.querySelector('.form');
+var headerFormButton = headerForm.querySelector('.form__submit');
+
+var headerFormNameLabel = headerForm.querySelector('.form__label--name');
+var headerFormNameInput = headerFormNameLabel.querySelector('.form__control');
+
+var headerFormPhoneLabel = headerForm.querySelector('.form__label--tel');
+var headerFormPhoneInput = headerFormPhoneLabel.querySelector('.form__control');
+
+var myStorage = window.localStorage;
+
 pageHeader.classList.remove('page-header--nojs');
 pageHeader.classList.add('page-header--closed');
 
+function menuOpen() {
+  pageHeader.classList.remove('page-header--closed');
+  pageHeader.classList.add('page-header--opened');
+  pageBody.style.height = '100vh';
+  pageBody.style.overflowY = 'hidden';
+}
+
+function menuClose() {
+  pageHeader.classList.add('page-header--closed');
+  pageHeader.classList.remove('page-header--opened');
+  pageBody.style.height = '';
+  pageBody.style.overflowY = '';
+}
+
 headerToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-    pageBody.style.height = '100vh';
-    pageBody.style.overflowY = 'hidden';
+    menuOpen()
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-    pageBody.style.height = '';
-    pageBody.style.overflowY = '';
+    menuClose()
   }
 });
 
 headerLinks.forEach((item) => {
-  item.addEventListener('click', function () {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-    pageBody.style.height = '';
-    pageBody.style.overflowY = '';
-  });
+  item.addEventListener('click', menuClose);
 });
+
+function submitHandler(evt) {
+  myStorage.setItem('name', headerFormNameInput.value);
+  myStorage.setItem('phone', headerFormPhoneInput.value);
+  console.log(evt);
+}
+
+headerFormButton.addEventListener('click', submitHandler);
